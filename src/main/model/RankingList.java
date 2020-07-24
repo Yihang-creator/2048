9b2e413e133008b793389295f95fe8feeba34651
Yihang-creator <64096168+Yihang-creator@users.noreply.github.com>
2020-07-24 10:55:00 -0700

    specify ,write tests and implement part of RankingList

--- src/main/model/RankingList.java
+++ src/main/model/RankingList.java
@@ -1,4 +1,69 @@
 package model;
 
+import java.util.ArrayList;
+
 public class RankingList {
+
+    private ArrayList<String> listOfPlayerNames;
+    private ArrayList<Integer> listOfScores;
+
+    public RankingList() {
+        listOfPlayerNames = new ArrayList();
+        listOfScores      = new ArrayList();
+    }
+
+    //REQUIRES: listOfPlayerNames and listOfScores are non-empty
+    //MODIFIES: this
+    //EFFECTS: sort listOfScores from high to low, change listOfPlayerNames accordingly
+    public void sortRankingList() {
+        for (int i = 0; i < listOfScores.size() - 1; i++) {
+            for (int j = listOfScores.size() - 1; j > i; j--) {
+                if (listOfScores.get(j) > listOfScores.get(j - 1)) {
+                    int smallerNum = listOfScores.get(j - 1);
+                    listOfScores.set(j - 1, listOfScores.get(j));
+                    listOfScores.set(j, smallerNum);
+                    String smallerNumName = listOfPlayerNames.get(j - 1);
+                    listOfPlayerNames.set(j - 1, listOfPlayerNames.get(j));
+                    listOfPlayerNames.set(j, smallerNumName);
+                }
+            }
+        }
+
+    }
+
+    //EFFECTS: return the name of the highest ranking
+    public String getHighestName() {
+        sortRankingList();
+        return listOfPlayerNames.get(0);
+    }
+
+    //EFFECTS: return the score of the highest ranking
+    public Integer getHighestScore() {
+        sortRankingList();
+        return listOfScores.get(0);
+    }
+
+    //MODIFIES: this
+    //EFFECTS: add one ranking to the rankinglist
+    public void addRanking(OneRanking oneRanking) {
+        listOfPlayerNames.add(oneRanking.getName());
+        listOfScores.add(oneRanking.getScore());
+
+    }
+
+
+    //EFFECTS; return the listOfScores
+    public ArrayList<Integer> getListOfScores() {
+        return listOfScores;
+
+    }
+
+    //EFFECTS; return the listOfScores
+    public ArrayList<String> getListOfPlayerNames() {
+        return listOfPlayerNames;
+
+    }
+
+
+
 }
--- src/test/model/TestRankingList.java
+++ src/test/model/TestRankingList.java
@@ -1,4 +1,80 @@
 package model;
 
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
 public class TestRankingList {
+    RankingList rankingList;
+    OneRanking oneRanking1;
+    OneRanking oneRanking2;
+    OneRanking oneRanking3;
+    OneRanking oneRanking4;
+    OneRanking oneRanking5;
+
+    @BeforeEach
+    public void setUp() {
+        rankingList = new RankingList();
+        oneRanking1 = new OneRanking();
+        oneRanking1.setScore(9998);
+        oneRanking1.setName("Player1");
+        oneRanking2 = new OneRanking();
+        oneRanking2.setScore(20000);
+        oneRanking2.setName("Player2");
+        oneRanking3 = new OneRanking();
+        oneRanking3.setScore(2000);
+        oneRanking3.setName("Player3");
+        oneRanking4 = new OneRanking();
+        oneRanking4.setScore(5004);
+        oneRanking4.setName("Player4");
+        oneRanking5 = new OneRanking();
+        oneRanking5.setScore(30488);
+        oneRanking5.setName("Player5");
+    }
+
+
+    @Test
+    public void RankingListTest() {
+        assertEquals(0,rankingList.getListOfPlayerNames().size());
+        assertEquals(0,rankingList.getListOfScores().size());
+    }
+
+    @Test
+    public void sortRankingListTest() {
+        rankingList.addRanking(oneRanking1);
+        rankingList.sortRankingList();
+        assertEquals("Player1",rankingList.getListOfPlayerNames().get(0));
+        assertEquals(9998,rankingList.getListOfScores().get(0));
+        rankingList.addRanking(oneRanking2);
+        rankingList.addRanking(oneRanking3);
+        rankingList.addRanking(oneRanking4);
+        rankingList.addRanking(oneRanking5);
+        rankingList.sortRankingList();
+        assertEquals("Player5",rankingList.getListOfPlayerNames().get(0));
+        assertEquals(30488,rankingList.getListOfScores().get(0));
+        assertEquals("Player2",rankingList.getListOfPlayerNames().get(1));
+        assertEquals(20000,rankingList.getListOfScores().get(1));
+        assertEquals("Player1",rankingList.getListOfPlayerNames().get(2));
+        assertEquals(9998,rankingList.getListOfScores().get(2));
+        assertEquals("Player4",rankingList.getListOfPlayerNames().get(3));
+        assertEquals(5004,rankingList.getListOfScores().get(3));
+        assertEquals("Player3",rankingList.getListOfPlayerNames().get(4));
+        assertEquals(2000,rankingList.getListOfScores().get(4));
+    }
+
+    //EFFECTS: return the name of the highest ranking
+    public void getHighestNameTest() {
+
+    }
+
+    //EFFECTS: return the score of the highest ranking
+    public void getHighestScoreTest() {
+
+    }
+
 }
